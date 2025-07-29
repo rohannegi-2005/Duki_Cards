d84890071c6dc2ebd388578de49d190dcc8e67fb
rohannegi-2005 <151851829+rohannegi-2005@users.noreply.github.com>
2025-07-29 12:22:21 +0530

    Update main.py
    adding timer for autopassing

--- main.py
+++ main.py
@@ -195,6 +195,19 @@ if st.session_state.player_name and (st.session_state.game_started or get_game(s
                     update_game_field(st.session_state.room_code, "current_turn", last_player)
 
         # If player click on Pass...
+        start = time.time()
+        auto_pass = False
+        while auto_pass != True:
+            time_passed = (time.time() - start)
+            if time_passed > 30:
+                auto_pass = True
+        if auto_pass = True:
+            mark_player_pass(st.session_state.room_code, st.session_state.player_id)      
+            idx = game["turn_order"].index(current_turn)
+            next_pid = game["turn_order"][(idx + 1) % len(game["turn_order"])]
+            update_game_field(st.session_state.room_code, "current_turn", next_pid)
+            st.session_state.selected_cards = []
+            st.rerun()
         with col2:
             if st.button("❌ Pass"):
                 mark_player_pass(st.session_state.room_code, st.session_state.player_id)      
